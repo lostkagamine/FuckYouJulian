a3397de21d51cfb60bb5534b6e758b61ee2f2b0c
Luna <ry00001@protonmail.com>
2023-02-21 01:15:48 +0000

    i love reflection

--- AnnoyTheFuckOutOfJulian/Program.cs
+++ AnnoyTheFuckOutOfJulian/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using AnnoyTheFuckOutOfJulian.LightModes;
 
 namespace AnnoyTheFuckOutOfJulian;
@@ -9,7 +10,28 @@ public static class Program
 
     public static void Main(string[] args)
     {
+        if (args.Length >= 1)
+        {
+            var type = args[0];
+            var asm = Assembly.GetExecutingAssembly();
+            var ty = asm.GetType($"AnnoyTheFuckOutOfJulian.LightModes.{type}");
+            if (ty == null)
+            {
+                throw new Exception("eat shit, type invalid");
+            }
+
+            if (!ty.IsAssignableTo(typeof(LightMode)))
+            {
+                throw new Exception("get fucked, type not assignable to LightMode");
+            }
+
+            var inst = (LightMode?)Activator.CreateInstance(ty) ?? throw new Exception("fuck");
+
+            mode = inst;
+        }
+        
         Console.WriteLine("fuck you julian");
+        Console.WriteLine($"initialising lightmode type {mode.GetType().Name}");
 
         var a = new Stopwatch();
         var total = 0.0;
